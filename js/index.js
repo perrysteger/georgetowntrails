6d88cfc28910a3a3eada6c8ee702cd51c9f40c87
tbbmtb <114437575+tbbmtb@users.noreply.github.com>
2022-10-05 12:34:53 -0500

    more edits

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
-const hamburgerBtn = document.getElementById('hamburger')
-const navList = document.getElementById('nav-list')
+const navToggle = document.querySelector(".navbar_toggle");
+const links = document.querySelector(".main_nav");
 
-function toggleButton() {
-  navList.classList.toggle('show')
-}
-
-hamburgerBtn.addEventListener('click', toggleButton)
+navToggle.addEventListener('click', function(){
+    links.classList.toggle("show_nav");
+})
