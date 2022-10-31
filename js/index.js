45e8104e2acb135f7ee6eee0e83ad3a418e1682f
tbbmtb <114437575+tbbmtb@users.noreply.github.com>
2022-10-31 16:42:44 -0500

    many changes

--- js/index.js
+++ js/index.js
@@ -4,3 +4,20 @@ const links = document.querySelector(".main_nav");
 navToggle.addEventListener('click', function(){
     links.classList.toggle("show_nav");
 })
+
+function sendEmail() {
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "",
+        Password : "",
+        To : '',
+        From : document.getElementById("email").value,
+        Subject : "New Message from GTF Website",
+        Body : "Name: " + document.getElementById("name").value
+            + "<br> Email: " + document.getElementById("email").value
+            + "<br> Phone: " + document.getElementById("phone").value
+            + "<br> Message: " + document.getElementById("message").value
+    }).then(
+      message => alert("Message Sent Successfully")
+    );
+}
