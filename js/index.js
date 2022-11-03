a5f7aa40027d499188e68812e4c2586f68a0f0d5
tbbmtb <114437575+tbbmtb@users.noreply.github.com>
2022-11-03 15:27:37 -0500

    more changes

--- js/index.js
+++ js/index.js
@@ -5,19 +5,48 @@ navToggle.addEventListener('click', function(){
     links.classList.toggle("show_nav");
 })
 
-function sendEmail() {
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "georgetowntrails@gmail.com",
-        Password : "18D92D3B8DBBA7B4C3514251BF64083681B2",
-        To : 'georgetowntrails@gmail.com',
-        From : "georgetowntrails@gmail.com", //document.getElementById("email").value,
-        Subject : "New Message from GTF Website",
-        Body : "Name: " + document.getElementById("name").value
-            + "<br> Email: " + document.getElementById("email").value
-            + "<br> Phone: " + document.getElementById("phone").value
-            + "<br> Message: " + document.getElementById("message").value
-    }).then(
-      message => alert("Message Sent Successfully")
-    );
-}
+// Elastic email/SMTPjs test
+
+// function sendEmail() {
+//     Email.send({
+//         Host : "smtp.elasticemail.com",
+//         Username : "georgetowntrails@gmail.com",
+//         Password : "18D92D3B8DBBA7B4C3514251BF64083681B2",
+//         To : 'georgetowntrails@gmail.com',
+//         From : document.getElementById("email").value,
+//         Subject : "New Message from GTF Website",
+//         Body : "Name: " + document.getElementById("name").value
+//             + "<br> Email: " + document.getElementById("email").value
+//             + "<br> Phone: " + document.getElementById("phone").value
+//             + "<br> Message: " + document.getElementById("message").value
+//     }).then(
+//       message => alert(message)
+//     );
+// }
+
+//EmailJS Test
+
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      phone: document.getElementById("phone").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "contact_service";
+    const templateID = "contact_form";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("phone").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
