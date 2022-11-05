f7c69cd38771caf99560b7d24e068951ef88f328
tbbmtb <114437575+tbbmtb@users.noreply.github.com>
2022-11-05 11:18:33 -0500

    more changes

--- js/index.js
+++ js/index.js
@@ -5,26 +5,6 @@ navToggle.addEventListener('click', function(){
     links.classList.toggle("show_nav");
 })
 
-// Elastic email/SMTPjs test
-
-// function sendEmail() {
-//     Email.send({
-//         Host : "smtp.elasticemail.com",
-//         Username : "georgetowntrails@gmail.com",
-//         Password : "18D92D3B8DBBA7B4C3514251BF64083681B2",
-//         To : 'georgetowntrails@gmail.com',
-//         From : document.getElementById("email").value,
-//         Subject : "New Message from GTF Website",
-//         Body : "Name: " + document.getElementById("name").value
-//             + "<br> Email: " + document.getElementById("email").value
-//             + "<br> Phone: " + document.getElementById("phone").value
-//             + "<br> Message: " + document.getElementById("message").value
-//     }).then(
-//       message => alert(message)
-//     );
-// }
-
-//EmailJS Test
 
 function sendMail() {
     var params = {
