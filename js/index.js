43f93e125e5e9f8e81b76d83d8dabd3b6b6b6477
tbbmtb <114437575+tbbmtb@users.noreply.github.com>
2023-02-14 15:37:13 -0600

    Replace EmailJS Contact form with Mailchimp form

--- js/index.js
+++ js/index.js
@@ -30,27 +30,29 @@
 })(jQuery); // end jQuery
 
 
-function sendMail() {
-    var params = {
-      name: document.getElementById("name").value,
-      phone: document.getElementById("phone").value,
-      email: document.getElementById("email").value,
-      message: document.getElementById("message").value,
-    };
+// Below is code for EmailJS service which isn't being used
+
+// function sendMail() {
+//     var params = {
+//       name: document.getElementById("name").value,
+//       phone: document.getElementById("phone").value,
+//       email: document.getElementById("email").value,
+//       message: document.getElementById("message").value,
+//     };
   
-    const serviceID = "contact_service";
-    const templateID = "contact_form";
+//     const serviceID = "contact_service";
+//     const templateID = "contact_form";
   
-      emailjs.send(serviceID, templateID, params)
-      .then(res=>{
-          document.getElementById("name").value = "";
-          document.getElementById("phone").value = "";
-          document.getElementById("email").value = "";
-          document.getElementById("message").value = "";
-          console.log(res);
-          alert("Your message sent successfully!!")
+//       emailjs.send(serviceID, templateID, params)
+//       .then(res=>{
+//           document.getElementById("name").value = "";
+//           document.getElementById("phone").value = "";
+//           document.getElementById("email").value = "";
+//           document.getElementById("message").value = "";
+//           console.log(res);
+//           alert("Your message sent successfully!!")
   
-      })
-      .catch(err=>console.log(err));
+//       })
+//       .catch(err=>console.log(err));
   
-  }
+//   }
